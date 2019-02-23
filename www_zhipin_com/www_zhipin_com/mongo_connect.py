945a76daa26061fa439d9c47359b2435153c5d37
youngsheng <18723431793@163.com>
2019-02-23 17:06:48 +0800

    update Proxy

--- www_zhipin_com/www_zhipin_com/mongo_connect.py
+++ www_zhipin_com/www_zhipin_com/mongo_connect.py
@@ -7,7 +7,7 @@ from bson import binary
 import random
 
 headers = {
-    'x-devtools-emulate-network-conditions-client-id': "5f2fc4da-c727-43c0-aad4-37fce8e3ff39",
+    #'x-devtools-emulate-network-conditions-client-id': "5f2fc4da-c727-43c0-aad4-37fce8e3ff39",
     'upgrade-insecure-requests': "1",
     'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
     'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
@@ -16,8 +16,30 @@ headers = {
     'accept-language': "zh-CN,zh;q=0.8,en;q=0.6",
     # 'cookie': "__c=1501326829; lastCity=101020100; __g=-; __l=r=https%3A%2F%2Fwww.google.com.hk%2F&l=%2F; __a=38940428.1501326829..1501326829.20.1.20.20; Hm_lvt_194df3105ad7148dcf2b98a91b5e727a=1501326839; Hm_lpvt_194df3105ad7148dcf2b98a91b5e727a=1502948718; __c=1501326829; lastCity=101020100; __g=-; Hm_lvt_194df3105ad7148dcf2b98a91b5e727a=1501326839; Hm_lpvt_194df3105ad7148dcf2b98a91b5e727a=1502954829; __l=r=https%3A%2F%2Fwww.google.com.hk%2F&l=%2F; __a=38940428.1501326829..1501326829.21.1.21.21",
     'cache-control': "no-cache",
-    'postman-token': "76554687-c4df-0c17-7cc0-5bf3845c9831"
+    #'postman-token': "76554687-c4df-0c17-7cc0-5bf3845c9831"
 }
+
+
+# 代理服务器
+proxyHost = "http-dyn.abuyun.com"
+proxyPort = "9020"
+
+# 代理隧道验证信息
+proxyUser = "H010H5D8769OA3PD"
+proxyPass = "B0976536FAB798D6"
+
+proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+    "host" : proxyHost,
+    "port" : proxyPort,
+    "user" : proxyUser,
+    "pass" : proxyPass,
+}
+
+proxies = {
+    "http"  : proxyMeta,
+    "https" : proxyMeta,
+}
+
 conn = MongoClient('127.0.0.1', 27017)
 db = conn.Jobs  # 连接mydb数据库，没有则自动创建
 
@@ -29,8 +51,18 @@ def init():
             continue
         detail_url = "https://www.zhipin.com/job_detail/%s.html?ka=search_list_1" % item['pid']
         print(detail_url)
-        result = requests.Session()
-        html = result.get(detail_url, headers=headers)
+        #result = requests.Session()
+        #html = result.get(detail_url, headers=headers)
+        try:
+            html = requests.get(detail_url, headers=headers, proxies=proxies)
+        except requests.exceptions.ProxyError as e:
+            if repr(e).find('402') != -1:
+                break
+            else:
+                print(e)
+        #else:
+        #    print('other orrer')
+
         if html.status_code == 404:
             item['detail'] = 'job has been taken'
             item['location'] = '-'
@@ -61,7 +93,8 @@ def init():
         # item['logo'] = binary.Binary(data)
         res = update(item)  # 保存数据
         print(res)
-        time.sleep(random.randint(50,90))  # 停停停
+        #time.sleep(random.randint(50,90))  # 停停停
+        time.sleep(0.2)  # 停停停
 
 
 # 保存数据到 MongoDB 中
